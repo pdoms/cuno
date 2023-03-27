5f9fe999765608b9d2c9ac3482547c626e2fee7f
pdoms <domspaulo@gmail.com>
2023-03-27 21:01:31 +0200

    almost established rudimentary gameplay

--- README.md
+++ README.md
@@ -5,9 +5,10 @@ Trying to learn C. A card game seemed like a good excercise to do so.
 ## Plans:
 
 - [x] set up project
-- [ ] establish rudimentary gameplay  
-- [ ] organize project structure  
-- [ ] as cli game  
+- [ ] establish rudimentary gameplay as cli game - by round
+- [ ] organize project structure/error handling
+- [ ] add tests [I was a bit overexcited at the beginning that I just played around and didn't write unit tests, don't like that]
+- [ ] establish game bot strategies - not just highest valued card
 - [ ] add graphical user interface - either with game engine or as in browser game - maybe both
 
 ## Compile
--- main.c
+++ main.c
@@ -4,13 +4,12 @@
 #include<string.h>
 #include<time.h>
 
-#define DEBUG 1
 
 #define DECK_SIZE 108
 #define COLOR_SIZE 13
 #define RAND_M ((1U << 31) - 1)
 #define NUM_PLAYER 4
-#define HAND_SIZE 7
+#define HAND_SIZE 2
 #define HAND_CAP HAND_SIZE*HAND_SIZE
 #define MAX_PLAYERS 8
 #define FLAG_CAP 8 
@@ -21,7 +20,7 @@
 int rand();
 void seed();
 int rseed = 0;
-
+int DEBUG = 0;
 
 inline void seed(int x) {
     rseed = x;
@@ -48,6 +47,8 @@ typedef enum ACTION_CARDS {
     CHOOSE_COLOR,
     DRAW_FOUR,
 } ACTION_CARDS;
+
+
 
 typedef enum LOCATION {
     DECK,
@@ -109,6 +110,17 @@ typedef struct Table {
 } Table;
 
 
+void insertion_sort_cards(Card* cards[], int n) {
+    for (int i = 1; i < n; i++) {
+        Card* tmp = cards[i];
+        int j = i - 1;
+        while (tmp->value < cards[j]->value && j >= 0) {
+            cards[j + 1] = cards[j];
+        --j;
+        }
+        cards[j + 1] = tmp;
+    }
+}
 void construct_deck() {
     int i = 0;
     int cards = 0;
@@ -244,7 +256,7 @@ void print_deck() {
 
 
 void shuffle_deck() {
-    printf("[DEBUG] shuffling deck\n");
+    printf("[GAME] shuffling deck\n");
     int i = DECK_SIZE - 1;
     int j;
     for (i; i >= 1; i--) {
@@ -256,7 +268,7 @@ void shuffle_deck() {
 }
 
 void deal_hands(Table *table) {
-    printf("[DEBUG] dealing hands\n");
+    printf("[GAME] dealing hands\n");
     int i = 0;
     for (i; i < table->num_players; i++) {
         int j = 0;
@@ -270,6 +282,70 @@ void deal_hands(Table *table) {
     }
 }
 
+
+int get_card_idx_from_hand(Hand* hand, Card* card_ptr) {
+    int i = 0;
+    for (i; i < hand->num_cards; i++) {
+        if (card_ptr == hand->cards[i]) {
+            return i;         
+        }
+    }
+    return hand->num_cards;
+}
+
+int player_next_best_card_idx(Table* table) {
+    Hand* hand = table->current_hand;
+    int len = hand->num_cards;
+    Card* possible_cards[len];
+
+    int i = 0;
+    int size = 0;
+    for (i; i < len; i++) {
+        Card* card = hand->cards[i];
+        if (card->color == table->current_color 
+                || card->value == table->current_value) {
+            possible_cards[size] = card;
+            size++;
+        }
+    }
+    if (size == 0) {
+        return len;
+    }
+    if (size == 1) {
+        return get_card_idx_from_hand(hand, possible_cards[0]);       
+    } else {
+        insertion_sort_cards(possible_cards, size);
+        return get_card_idx_from_hand(hand, possible_cards[size-1]);
+    }
+}
+
+void print_player_hand(Table* table, int suggestion) {
+
+    Hand* hand = table->current_hand;
+    printf("+++ Player %d's Hand", hand->player_id);
+    if (hand->is_active == 1) {
+        printf(" (active)\n");
+    } else {
+        printf("\n");
+    }
+    
+    int i = 0;
+    int sugg;
+    if (suggestion == 1) {
+        sugg = player_next_best_card_idx(table);
+    }
+    for (i;i<hand->num_cards;i++) {
+        printf("\t#%d -> color: %s; value: %s", i+1, 
+                print_color(hand->cards[i]->color), 
+                print_value(hand->cards[i]->value));
+        if (suggestion == 1 && i == sugg) {
+            printf("\t\t<-\n");
+        } else {
+            printf("\n");
+        }
+    }
+}
+
 void print_hand(Hand *hand) {
     printf("+++ Player %d's Hand", hand->player_id);
     if (hand->is_active == 1) {
@@ -277,12 +353,12 @@ void print_hand(Hand *hand) {
     } else {
         printf("\n");
     }
+    
     int i = 0;
     for (i;i<hand->num_cards;i++) {
         printf("\t#%d -> color: %s; value: %s\n", i+1, 
                 print_color(hand->cards[i]->color), 
                 print_value(hand->cards[i]->value));
-
     }
 }
 
@@ -297,7 +373,7 @@ Discards* init_discards() {
 
 //should be called only once
 void draw_to_discard(Table* table) {
-    printf("[DEBUG] drawing initial discard\n");
+    printf("[GAME] drawing initial discard\n");
     int top = table->discarded->len;
     table->discarded->cards[top] = &deck[table->top_of_deck]; 
     table->current_color = deck[table->top_of_deck].color;
@@ -343,6 +419,7 @@ Table set_table(int num_players, int actives) {
     }
     deal_hands(&table);
     draw_to_discard(&table);
+    table.current_hand = table.player_hands[0];
     return table;
 }
 
@@ -376,25 +453,19 @@ void change_direction(Table* table) {
 }
 
 void set_current_player(Table* table) {
-    printf("changing from %d", table->current_player);
     if (table->dir == CLOCKWISE) {
-        printf("DIRECTION is CLOCKWISE\n");
         if (table->current_player < table->num_players) {
             table->current_player = table->current_player + 1;
         } else {
             table->current_player = 1;
         }
     } else {
-        printf("DIRECTION is COUNTERCLOCKWISE\n");
-        printf("NUM PLAYERS %d", table->num_players);
         if (table->current_player == 1) {
-            printf("SETTING TO LAST PLAYER\n");
             table->current_player = table->num_players;
         } else {
             table->current_player = table->current_player-1;
         }
     }
-    printf(" to %d\n", table->current_player);
 }
 
 Card* get_next_card(Table* table) {
@@ -413,7 +484,6 @@ void shift_cards(Hand* hand, int card_idx) {
 }
 
 Card* draw_to_player(Table* table, int player_id) {
-    printf("[DEBUG] drawing card for player %d\n", player_id);
     Hand* hand = get_player_hand(table, player_id);
     Card* card = get_next_card(table);
     card->player_id = player_id;
@@ -422,16 +492,30 @@ Card* draw_to_player(Table* table, int player_id) {
     return card;
 }
 
-void player_discard(Table* table, int player_id, int card_idx) {
+
+
+int player_discard(Table* table, int player_id, int card_idx) {
+
     Hand* hand = get_player_hand(table, player_id);
+    
     int top = table->discarded->len;
-    table->discarded->cards[top] = hand->cards[card_idx]; 
-    table->current_color = table->discarded->cards[top]->color;
-    table->current_value = table->discarded->cards[top]->value;
-    deck[table->top_of_deck].location = DISCARD;
-    table->discarded->len++;
-    table->top_of_deck++;
-    shift_cards(hand, card_idx);
+    Card* card = hand->cards[card_idx];
+    Card* current_discard = table->discarded->cards[top-1];
+
+    if (card->color == current_discard->color 
+            || card->value == current_discard->value
+            || card->color == BLACK
+            ) {
+        table->discarded->cards[top] = hand->cards[card_idx]; 
+        table->current_color = table->discarded->cards[top]->color;
+        table->current_value = table->discarded->cards[top]->value;
+        deck[table->top_of_deck].location = DISCARD;
+        table->discarded->len++;
+        table->top_of_deck++;
+        shift_cards(hand, card_idx);
+        return 0;
+    }
+    return 1;
 }
 
 
@@ -486,17 +570,6 @@ int hand_has_zero(Hand* hand) {
     return hand->num_cards;
 }
 
-void insertion_sort_cards(Card* cards[], int n) {
-    for (int i = 1; i < n; i++) {
-        Card* tmp = cards[i];
-        int j = i - 1;
-        while (tmp->value < cards[j]->value && j >= 0) {
-            cards[j + 1] = cards[j];
-        --j;
-        }
-        cards[j + 1] = tmp;
-    }
-}
 
 int get_card_idx(Table* table, Card* card_ptr) {
     Hand* hand = table->current_hand;
@@ -507,6 +580,24 @@ int get_card_idx(Table* table, Card* card_ptr) {
         }
     }
     return hand->num_cards;
+}
+
+int has_card_value(Hand *hand, int value) {
+    int i =0;
+    for (i; i < hand->num_cards; i++) {
+        if (hand->cards[i]->value == value) {
+            return i;
+        }
+    }
+    return hand->num_cards;
+}
+
+int check_if_can_stack(Table *table) {
+    Card *card = table->discarded->cards[table->discarded->len];
+    Hand *hand = table->current_hand;
+    int check = has_card_value(hand, card->value);
+    return check;
+
 }
 
 void bot_best_next_card(Table* table, int player_id) {
@@ -533,14 +624,9 @@ void bot_best_next_card(Table* table, int player_id) {
             size++;
         }
     }
-
-    print_hand(hand);
-    if (size > 0) {
-        printf("found these %d cards:\n", size);
-        for (int j = 0; j < size; j++) {
-            print_card(possible_cards[j], 0);
-        }
-    }   
+    if (DEBUG == 1) {
+        print_hand(hand);
+    }
     if (size == 1) {
         int idx = get_card_idx(table, possible_cards[0]);
         player_discard(table, table->current_player, idx);
@@ -548,18 +634,21 @@ void bot_best_next_card(Table* table, int player_id) {
     }
 
     if (size > 1) {
-        printf("sorting...\n");
         insertion_sort_cards(possible_cards, size);
-        print_card(possible_cards[size-1], 0);
+        if (DEBUG == 1) {
+            printf("Will play\n");
+            print_card(possible_cards[size-1], 1);
+        }
         int idx = get_card_idx(table, possible_cards[size-1]);
-        printf("idx %d\n", idx);
         player_discard(table, table->current_player, idx);
         return;
         
     }
 
     if (size == 0) {
+        printf("Will draw\n");
         draw_to_player(table, table->current_player);
+        return;
     }
     
 
@@ -600,7 +689,7 @@ void bot_best_next_color(Table* table) {
     int max = 0;
     int next_clr = 0;
     for (i; i < COLORS; i++) {
-        if clrs[i] > max {
+        if (clrs[i] > max) {
             max = clrs[i];
             next_clr = i;
         }
@@ -663,12 +752,11 @@ int c_to_int(char c) {
 }
 
 void print_delim() {
-    printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+    printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n");
 }
 void print_action(ACTION act) { 
     switch (act) {
         case NEXT_PLAYER:
-            printf("NEXT PLAYER\n");
             break;
         case NEXT_DRAW2:
             printf("NEXT DRAW 2\n");
@@ -688,14 +776,78 @@ void print_action(ACTION act) {
     }
 }
 
+int is_uno(Table *table) {
+    Hand *hand = table->current_hand;
+    if (hand->num_cards == 1) {
+        print_delim();
+        printf("=>=>=> Player %d says UNO!\n", table->current_player);
+        print_delim();
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+
+    //NEXT_PAUSE = 10,
+    //REVERSE,
+    //DRAW_TWO,
+    //CHOOSE_COLOR,
+    //DRAW_FOUR,
+
+
+int calculate_hands(Table *table, int winner) {
+    printf("Hand Values:\n\n");
+    int sums[table->num_players];
+    memset(sums, 0, table->num_players*sizeof(int));
+    int i = 0;
+    int total = 0;
+    for (i; i <table->num_players;i++) {
+        Hand* hand = table->player_hands[i];
+        int j = 0;
+        for (j; j < hand->num_cards;j++) {
+            Card* card = hand->cards[j];
+            if (card->value < 10) {
+                sums[i] += card->value;
+            } else if (card-> value < 13) {
+                sums[i] += 20;
+            } else {
+                sums[i] += 50;
+            }
+        }
+        if (i + 1 != winner) {
+            printf("\tPlayer %d: %d\n", i+1, sums[i]);
+        }
+        total += sums[i];
+    }
+    return total;
+}
+
+int player_has_won(Table *table) {
+    Hand *hand = table->current_hand;
+    if (hand->num_cards == 0) {
+        print_delim();
+        printf("=>=>=> Player %d has Won!\n", table->current_player);
+        int score = calculate_hands(table, hand->player_id);
+        printf("\n\tPlayer %d's score: %d\n", table->current_player, score);
+        print_delim();
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 int main(int argc, char **argv) {
-    //TODO general todo, think of best way to sort a hand
     seed(time(NULL));
+    //TODO don't allow initial discad if WILD or WILD DRAW4
+    //TODO bug -> boundary error when draw card 
     //TODO before start determine who starts by drawing cards 
+    //TODO check if deck is exhausted
+    //TODO reshuffle when stack is exhausted
     int num_players = DEFAULT_PLAYERS;
     int num_active = DEFAULT_ACTIVE;
     char* PLAYER = "player";
-    char* ACTIVE = "active";
+    char* DEB = "-d";
     for (int i = 1; i < argc; ++i)
     {   
         char flag[FLAG_CAP];
@@ -703,29 +855,33 @@ int main(int argc, char **argv) {
         parse_arg(argv[i], flag, value);
         if (streq(flag, PLAYER)) {
             num_players=c_to_int(value[0]);
-        } else if (streq(flag, ACTIVE)) {
-            num_active=c_to_int(value[0]);
+        } else if (streq(flag, DEB)) {
+            DEBUG = 1; 
         } else {
             printf("UNKNOWN ARGUMENT - %s\n", argv[i]);
         }
     }   
-    printf("[DEBUG] NUM PLAYERS: %d\n", num_players);
-    printf("[DEBUG] NUM ACTIVE: %d\n", num_active);
+    if (DEBUG == 1) {
+        printf("[DEBUG] NUM PLAYERS: %d\n", num_players);
+        printf("[DEBUG] NUM ACTIVE: %d\n", num_active);
+    }
     Table table = set_table(num_players, num_active);
     int user_play_action;
-    int prev_player = 0;
-    Hand* previous_hand;
     Card* drawn_card;
     int run = 1;
     int next;
     int next_action;
     COLOR clr;
     int init = 1;
+    int skip_play = 0;
+
     while (run) {
         print_delim();
         print_discard(&table);
         next_action = determine_action(&table);
-        print_action(next_action);
+        if (DEBUG == 1) {
+            print_action(next_action);
+        }
         switch (next_action) {
             case CHANGE_DIR:
                 change_direction(&table);
@@ -746,12 +902,25 @@ int main(int argc, char **argv) {
                 }
                 set_current_player(&table);
                 break;
-            //TODO next draw but can play afterwards and doesn't have to draw    
             case NEXT_DRAW2:
                 set_current_player(&table);
                 if (table.current_hand->is_active == 1) {
-                    draw_to_player(&table, table.current_player);
-                    draw_to_player(&table, table.current_player);
+                    if (check_if_can_stack(&table) < table.current_hand->num_cards) {
+                        print_player_hand(&table, 0);
+                        printf("\t#0 -> draw 2 cards\n");
+                        printf("Do you want to play? ");
+                        scanf("%d", &user_play_action);
+                        if (user_play_action == 0) {
+                            draw_to_player(&table, table.current_player);
+                            draw_to_player(&table, table.current_player);
+                        } else {
+                            player_discard(&table, table.current_player, user_play_action-1);
+                            skip_play = 1;
+                        }  
+                    } else {
+                        draw_to_player(&table, table.current_player);
+                        draw_to_player(&table, table.current_player);
+                    }
                 } else {   
                     draw_to_player(&table, table.current_player);
                     draw_to_player(&table, table.current_player);
@@ -779,7 +948,6 @@ int main(int argc, char **argv) {
                     init = 0;
                     break;
                 }
-                printf("GOING TO NEXT PLAYER\n");
                 set_current_player(&table);
                 break;
         }
@@ -787,47 +955,65 @@ int main(int argc, char **argv) {
             break;
         }
         //print_discard(&table);
-        printf("Player %d's turn!\n", table.current_player);
-        set_current_hand(&table); 
-        if (table.current_hand->is_active == 1) {
-            print_hand(table.current_hand);
-            printf("\t#0 -> draw card\n");
-            printf("Which card do you want to play? ");
-            scanf("%d", &user_play_action);
-            if (user_play_action == 0) {
-                drawn_card = draw_to_player(&table, table.current_player);
-                printf("Drawn card: \n");
-                print_card(drawn_card, 0);
-                //TODO check if it is possible to player and if not, don't ask!!!
-                printf("Do you want to play this card? [0/1] ");
+        if (skip_play == 0) {
+            printf("Player %d's turn!\n", table.current_player);
+            set_current_hand(&table); 
+            if (table.current_hand->is_active == 1) {
+                print_player_hand(&table, 1);
+                printf("\t#0 -> draw card\n");
+                printf("Which card do you want to play? ");
                 scanf("%d", &user_play_action);
-                if (user_play_action == 1) {
-                    player_discard_drawn(&table, table.current_player);
+                if (user_play_action == 0) {
+                    drawn_card = draw_to_player(&table, table.current_player);
+                    printf("Drawn card: \n");
+                    print_card(drawn_card, 0);
+                    int topp = table.discarded->len;
+                    Card* current_discard = table.discarded->cards[topp-1];
+                    if (drawn_card->value == current_discard->value || drawn_card->color == current_discard->color) {
+                        printf("Do you want to play this card? [0/1] ");
+                        scanf("%d", &user_play_action);
+                        if (user_play_action == 1) {
+                            player_discard_drawn(&table, table.current_player); 
+                        }
+                    }
+                } else {
+                    printf("Player %d discards: \n", table.current_player);
+                    print_card(table.current_hand->cards[user_play_action-1], 1);
+                    int card_idx = user_play_action-1;
+                    while (player_discard(&table, table.current_player, card_idx)) {
+                        printf("\n\n========== ILLEGAL MOVE ===========\n");
+                        printf("\nCard cannot be discarded. Try again\n");
+                        printf("\n=====================================\n\n");
+                        print_player_hand(&table, 1);
+                        printf("\t#0 -> draw card\n");
+                        printf("Which card do you want to play? ");
+                        scanf("%d", &user_play_action);
+                        card_idx = user_play_action-1;
+                    }
                 }
+                printf("\n");
+                print_hand(table.current_hand);
+                print_delim();
+                printf("Next player? [0/1] ");
+                next = scanf("%d", &next);
             } else {
-                printf("Player %d discards: \n", table.current_player);
-                print_card(table.current_hand->cards[user_play_action-1], 1);
-                player_discard(&table, table.current_player, user_play_action-1);
-            }
-            printf("\n");
-            print_hand(table.current_hand);
-            printf("Next player? [0/1] ");
-            next = scanf("%d", &next);
-            if (next == 1) {
-                prev_player = table.current_player;
-                previous_hand = get_player_hand(&table, table.current_player);
-            } else {
-               run = 0;
+                printf("Playing %d's hand.\n", table.current_player);
+                bot_best_next_card(&table, table.current_player);
+                print_delim();
+                print_discard(&table);
+                printf("Next player? [0/1] ");
+                next = scanf("%d", &next);
             }
         } else {
-            printf("Playing computer hand...\n");
-            bot_best_next_card(&table, table.current_player);
-            printf("Next player? [0/1] ");
-            next = scanf("%d", &next);
+            skip_play = 0;
+        }
+        is_uno(&table);
+        if (player_has_won(&table) == 1) {
+            run = 0;
         }
     }
 
     free_table(&table);
-    printf("Ok, bye!");
+    printf("Bye!");
     return 0;
 }
